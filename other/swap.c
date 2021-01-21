25fdb4c101abf242644b9883c74e6253d14812ed
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-01-21 22:05:31 +0300

    Create swap.c

---
+++ other/swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void swap(int a, int b);
+
+int main(void){
+    int x = 1, y = 2;
+    swap(x, y);
+    printf("x is %i\n", x);
+    printf("y is %i\n", y);
+}
+
+void swap(int a, int b){
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
