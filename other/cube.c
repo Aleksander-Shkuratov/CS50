d7f1f1f37dfe0ecd8ffacb6a4dcea322d6e563a7
ALEKSANDER <76268191+Aleksander-Shkuratov@users.noreply.github.com>
2021-01-21 21:07:58 +0300

    Create cube.c

---
+++ other/cube.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int cube(int input);
+
+int main(void){
+     int x = 2;
+     printf("x is %i\n", x);
+     x = cube(x);
+     printf("x is %i\n", x);
+}
+
+int cube(int input){
+     int output = input * input * input;
+     return output;
+}
