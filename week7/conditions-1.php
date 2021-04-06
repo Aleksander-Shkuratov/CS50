af2b269644353e92dc5292f57e67bcba7533438c
Aleksander Shkuratov <76268191+AleksanderShkuratov@users.noreply.github.com>
2021-04-06 12:41:13 +0300

    Create conditions-1.php

---
+++ week7/conditions-1.php
@@ -0,0 +1,15 @@
+<?php
+
+    $n = readline("I'd like an integer please: ");
+
+    if ($n > 0){
+        print("You picked a positive number!\n");
+    }
+    else if ($n == 0){
+        print("You pikted zero!\n");
+    }
+    else{
+        print("You pikted a negative number!\n");
+    }
+
+?>
