5cd1730c534bd394dcc9df2dfc87d01a54fb6fcb
Aleksander Shkuratov <76268191+AleksanderShkuratov@users.noreply.github.com>
2021-04-06 16:51:17 +0300

    Create array-2.php

---
+++ week7/array-2.php
@@ -0,0 +1,9 @@
+#!/usr/bin/env php
+<?php
+
+    $Arr = array ( "a" => 1, "b" => "two", "c" => "three");
+
+    foreach ($Arr as $key => $value) {
+        echo "Key: $key; Value: $value\n";
+    }
+?>
